7d2c6babb75b54d7a99744263389ff8d25631cba
L4er70 <laert.juti11@gmail.com>
2024-02-13 16:28:21 +0100

    play round built

---
+++ game.js
@@ -0,0 +1,47 @@
+console.log('Rock ,paper ,scissor');
+let myArray =['Rock','Paper', 'Scissors']
+/*
+const randomChoice = Math.floor(Math.random()*myArray.length)
+const choice = myArray[randomChoice];
+console.log(choice)
+*/
+
+function getComputerChoice(){
+    const randomChoice = Math.floor(Math.random()*myArray.length)
+    const choice = myArray[randomChoice];
+    return choice; 
+    
+    }
+    console.log('The computer choice is '+getComputerChoice())
+
+    const playerSelection = 'SCissors';
+    console.log('Users choice is '+ playerSelection);
+    const computerSelection= getComputerChoice();
+
+    
+    function playRound(playerSelection, computerSelection){
+        playerSelection= playerSelection.toLowerCase();
+        computerSelection= computerSelection.toLowerCase();
+        console.log('player: '+playerSelection);
+        console.log('pc :' + computerSelection);
+
+
+        if (playerSelection === computerSelection){
+            console.log("Draw");
+       
+    }else if((playerSelection==='rock' && computerSelection==='scissors') || 
+    (playerSelection==='paper' && computerSelection==='rock') || 
+    (playerSelection==='scissors' && computerSelection==='paper'))
+   
+     {
+        console.log('you win');
+
+    }
+    else {
+        console.log('You lose');
+    } 
+}
+playRound(playerSelection,computerSelection);
+
+
+
